405db2ef6cd00a405d6f05db016f18835dce3bbc
Asaad Belarbi <asaadbelarbi15@gmail.com>
2021-06-21 10:41:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,5 +22,5 @@ def predict():
 
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0')
